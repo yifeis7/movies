6d4abb421ff330700a81efc6aad2f32e5c0c942d
yifeis7 <yifeis7@illinois.edu>
2023-07-31 22:04:38 +0800

    add RestAPI

--- src/main/java/sss/movies/Movie.java
+++ src/main/java/sss/movies/Movie.java
@@ -1,10 +1,20 @@
 package sss.movies;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
 import java.util.List;
 @Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
+    @Id
     private ObjectId id;
     private String title;
     private String releaseDate;
@@ -12,6 +22,8 @@ public class Movie {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+    @DocumentReference
     private List<String> reviewIds;
+
 
 }
