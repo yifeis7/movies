7f5bcf7db888375b08dcfbaa08863bbf9d25809b
yifeis7 <yifeis7@illinois.edu>
2023-08-04 22:41:30 +0800

    finish backend

--- src/main/java/sss/movies/Movie.java
+++ src/main/java/sss/movies/Movie.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class Movie {
     @Id
     private ObjectId id;
+    private String imdbId;
     private String title;
     private String releaseDate;
     private String trailerLink;
--- src/main/java/sss/movies/MovieController.java
+++ src/main/java/sss/movies/MovieController.java
@@ -2,6 +2,7 @@ package sss.movies;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -17,12 +19,14 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
+
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
-    @GetMapping("/{id}")
-    public ResponseEntity<Movie> getSingleMovie(@PathVariable ObjectId id){
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
     }
 
 }
